99cc8836cf00a633a62604267f1969d66885e55e
Andrew Ribas <andrew.j.ribas@gmail.com>
2020-07-06 21:26:18 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,11 @@
-def hello_t(names){code}
-  yielding_with_args(names){block}
+def hello_t(names)
+  i = 0
+
+  while i < names.length
+    yield names[i]
+    i = i + 1
+  end
 end
+
 
 # call your method here!
